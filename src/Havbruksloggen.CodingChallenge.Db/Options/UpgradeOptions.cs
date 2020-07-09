65348aeca006fb222e1bb167a1120317b1946270
Bayram Ali Basgul <bayramali@outlook.com>
2020-07-09 17:29:37 +0200

    cleanings

--- src/Havbruksloggen.CodingChallenge.Db/Options/UpgradeOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace Havbruksloggen.CodingChallenge.Db.Options
-{
-    public class UpgradeOptions
-    {
-        public int CommandExecutionTimeoutSeconds { get; set; }
-        public string RunPattern { get; set; }
-    }
-}
